b92c90a0fbc0c5e7f9f2fda3d0a71123337f0291
dae <57477380+dkyc5402@users.noreply.github.com>
2024-08-01 17:50:13 +0800

    Updated Navbar items for client

--- front/src/components/Client_Navbar.js
+++ front/src/components/Client_Navbar.js
@@ -1,1 +1,216 @@
 // Navbar for client
+
+import Dell_logo from "../assets/dell_logo.svg";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import 'typeface-roboto';
+
+const pages = ["Workshop Request Form", "View Workshops"];
+const settings = ["Account", "Logout"];
+
+function ClientNavbar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static" sx={{ bgcolor: "white" }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="Dell Technologies Logo"
+                        sx={{
+                            borderRadius: 0,
+                            ".MuiTouchRipple-ripple .MuiTouchRipple-child": {
+                                borderRadius: 0,
+                            }, display: { xs: "none", md: "flex" }, mr: 1
+                        }}
+                    >
+                        <img
+                            src={Dell_logo}
+                            alt="Dell Technologies Logo"
+                            height="33"
+                        ></img>
+                    </IconButton>
+
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "none", md: "flex" },
+                            fontFamily: "Roboto",
+                            fontWeight: 300,
+                            fontSize: "1.4rem",
+                            color: '#0672cb',
+                            textDecoration: "none",
+                        }}
+                    >
+                        Workshop Resource Portal
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="#636363"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: "block", md: "none" },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">
+                                        <Link
+                                            to={page === "Home" ? "/client" : `/${page.toLowerCase()}`}
+                                            style={{ textDecoration: 'none', color: 'inherit' }}
+                                        >
+                                            {page}
+                                        </Link>
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="Dell Technologies Logo"
+                        sx={{
+                            borderRadius: 0,
+                            ".MuiTouchRipple-ripple .MuiTouchRipple-child": {
+                                borderRadius: 0,
+                                backgroundColor: "red",
+                            }, display: { xs: "flex", md: "none" }, mr: 1
+                        }}
+                    >
+                        <img
+                            src={Dell_logo}
+                            alt="Dell Technologies Logo"
+                            height="33"
+                        ></img>
+                    </IconButton>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "flex", md: "none" },
+                            flexGrow: 1,
+                            fontFamily: "Roboto",
+                            fontWeight: 300,
+                            fontSize: "1rem",
+                            color: '#0672cb',
+                            textDecoration: "none",
+                        }}
+                    >
+                        Workshop Resource Portal
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                data-testid={`${page.toLowerCase()}-button`}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: "#636363", display: "block" }}
+                            >
+                                <Link
+                                    to={page === "Home" ? "/client" : `/${page.toLowerCase()}`}
+                                    style={{ textDecoration: 'none', color: 'inherit' }}
+                                >
+                                    {page}
+                                </Link>
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: "45px" }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+
+export { ClientNavbar };
--- front/src/pages/Client.js
+++ front/src/pages/Client.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import FormPreview from '../components/FormPreview';
 import { FormProvider } from '../components/FormContext';
+import { ClientNavbar } from '../components/Client_Navbar';
 function App() {
       return (
         <div>
+          <ClientNavbar/>
           <h2>Client Page</h2>
           <FormProvider>
            <FormPreview/>
