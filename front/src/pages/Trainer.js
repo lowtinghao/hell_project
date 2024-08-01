01b1de7c65b6867085757d760637021f96c8805b
dae <57477380+dkyc5402@users.noreply.github.com>
2024-08-01 17:39:31 +0800

    Static Trainer Navbar

--- front/src/pages/Trainer.js
+++ front/src/pages/Trainer.js
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
+import { TrainerNavbar } from '../components/Trainer_Navbar';
 function App() {
       return (
         <div>
+          <TrainerNavbar/>
           <h2>Trainer Page</h2>
           <button><Link to="/">Back</Link></button>
         </div>
