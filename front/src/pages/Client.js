0a3499c9182cc5bd9468f21ce5ad50b92db9acdb
dae <57477380+dkyc5402@users.noreply.github.com>
2024-08-02 19:55:51 +0800

    Client Workshop table done. TODO: client id filtering

--- front/src/pages/Client.js
+++ front/src/pages/Client.js
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 import FormPreview from '../components/FormPreview';
 import { FormProvider } from '../components/FormContext';
 import { ClientNavbar } from '../components/Client_Navbar';
+import ViewClientWorkshopsTable from '../components/Client_ViewWorkshopsTable.js';
+
+
 function App() {
       return (
         <div>
@@ -10,6 +13,10 @@ function App() {
           <FormProvider>
            <FormPreview/>
           </FormProvider>
+          <br/>
+
+          <ViewClientWorkshopsTable/>
+
           <button><Link to="/">Back</Link></button>
         </div>
       );
